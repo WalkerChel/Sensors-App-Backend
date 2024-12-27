b463feacab2a264f0c03c1ae8f7405ee11a46c30
WalkerChel <illiakazlou28@gmail.com>
2024-12-28 00:08:45 +0300

    feat(procedures): db procedure to generate sensors

---
+++ migrations/000003_generate_sensors.up.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE PROCEDURE generate_sensors()
+LANGUAGE plpgsql
+AS $function$
+DECLARE
+    regionsNumber INT;
+    regId INT;
+    n INT;
+BEGIN
+  SELECT COUNT(DISTINCT id)
+  INTO regionsNumber
+  FROM regions;
+
+    FOR regId IN 1..regionsNumber LOOP
+        FOR n IN 1..floor(random() * regionsNumber + 1)::INT LOOP
+            INSERT INTO sensors (RegionId, Name, Longitude, Latitude) 
+            VALUES (
+                regId, 
+                substr(md5(random()::text), 1, 12), 
+                random() * 180 - 90, 
+                random() * 360 - 180
+            );
+        END LOOP;
+    END LOOP;
+END;
+$function$;
+
+CALL generate_sensors();
